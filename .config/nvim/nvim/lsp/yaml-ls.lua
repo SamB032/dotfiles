8e27749a591fb9f06eb396b154000bd221847446
Sam <sam.boffey04@gmail.com>
2025-06-09 19:38:19 +0100

    Remove duplicate nvim

--- .config/nvim/nvim/lsp/yaml-ls.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = { 'yaml-language-server', '--stdio' },  -- Start YAML LSP server
-  filetypes = { 'yaml', 'yml' },
-  root_markers = { '.git', '.yamllint', 'yamlls.config.json' },  -- Detect project root
-}
--- .config/nvim/nvim/lua/plugins/icons.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'nvim-tree/nvim-web-devicons',
-  },
-  {
-    'echasnovski/mini.nvim',
-    version = false
-  }
-}
